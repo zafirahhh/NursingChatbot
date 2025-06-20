41f0801bfc0ef473448cbffa2bccfd62abef96d8
RP+23049844@DESKTOP-1L5IPT1 <23049844@myrp.edu.sg>
2025-06-20 16:08:00 +0800

    update

--- test_chatbot.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:8000/search"
-data = {"query": "What is nursing care?"}
-
-response = requests.post(url, json=data)
-print("Status code:", response.status_code)
-print("Response:", response.json())
